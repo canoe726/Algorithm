f59e5e86bb0777084d8e7610338cf70064684e1c
canoe726 <canoe918@gmail.com>
2022-05-19 00:23:07 +0900

    최솟값 만들기

---
+++ 2021년/프로그래머스/Lv.2/최솟값_만들기.js
@@ -0,0 +1,10 @@
+function solution(A, B) {
+    let answer = 0;
+    
+    A = A.sort((a, b) => a - b);
+    B = B.sort((a, b) => b - a);
+    for (let i = 0; i < A.length; i++) {
+        answer += A[i] * B[i];
+    }
+    return answer;
+}

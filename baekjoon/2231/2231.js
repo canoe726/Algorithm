91c22715914b9b0277db85575ca563aa92dde083
youngkimEBC <youngkim@ent-bc.com>
2024-01-10 16:48:18 +0900

    feat: 2231

--- baekjoon/2231/2231.js
+++ baekjoon/2231/2231.js
@@ -1,6 +1,26 @@
-const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './test')
+const INPUT = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input')
   .toString()
   .trim();
 
-console.log(n, input);
+function main(input) {
+	const MAX_SIZE = Number(input)
+  let answer = 0
+
+	for (let i = 1; i <= MAX_SIZE; i++) {
+    let sum = i
+    sum += (i).toString().split('').reduce((acc, cur) => {
+      return acc + Number(cur)
+    }, 0)
+
+    if (sum === MAX_SIZE) {
+      answer = i
+      break;
+    }
+	}
+
+  return answer
+}
+
+const response = main(INPUT)
+console.log(response)

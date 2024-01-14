0f6a28c8df4f581fce4c1955a2d6e0f49e1d45c1
canoe726 <canoe918@gmail.com>
2024-01-14 15:20:53 +0900

    feat: 1182

--- baekjoon/brute-force/1182/1182.js
+++ baekjoon/brute-force/1182/1182.js
@@ -1,10 +1,49 @@
-const [n, ...arr] = require('fs')
+const [n, arr] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input')
   .toString()
   .trim()
   .split('\n');
+const [N, S] = n.split(' ');
+const numbers = arr.split(' ').map((a) => Number(a));
 
-function main(n, arr) {}
+function pickNumber(numbers, N, S, index, picked, answer) {
+  if (picked.length === N) {
+    const notPicked = !picked.some((p) => p === true);
+    if (notPicked) {
+      return;
+    }
 
-const response = main(n, arr);
+    let sum = 0;
+    for (let i = 0; i < picked.length; i++) {
+      if (picked[i]) {
+        sum += numbers[i];
+      }
+    }
+
+    if (sum === S) {
+      answer[0] += 1;
+    }
+
+    return;
+  }
+
+  picked.push(true);
+  pickNumber(numbers, N, S, index + 1, picked, answer);
+  picked.pop();
+
+  picked.push(false);
+  pickNumber(numbers, N, S, index + 1, picked, answer);
+  picked.pop();
+}
+
+function main(N, S, numbers) {
+  const answer = [0];
+  const picked = [];
+
+  pickNumber(numbers, N, S, 0, picked, answer);
+
+  return answer[0];
+}
+
+const response = main(Number(N), Number(S), numbers);
 console.log(response);

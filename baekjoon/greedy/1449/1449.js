c98496b794ced5104ff52e3c5a910233e1634d38
canoe726 <canoe918@gmail.com>
2024-01-14 18:16:26 +0900

    feat: 1449

--- baekjoon/greedy/1449/1449.js
+++ baekjoon/greedy/1449/1449.js
@@ -4,7 +4,35 @@ const [n, ...arr] = require('fs')
   .trim()
   .split('\n');
 
-function main(n, arr) {}
+const [N, L] = n.split(' ').map((n) => Number(n));
+const numbers = arr
+  .map((a) => a.split(' '))
+  .flat()
+  .map((a) => Number(a));
 
-const response = main(n, arr);
+function main(N, L, numbers) {
+  const numberList = numbers.sort((a, b) => a - b);
+  let answer = 0;
+
+  for (let i = 0; i < N; i++) {
+    const base = numberList[i];
+    answer += 1;
+
+    let isCovered = false;
+    const tapedBase = base + L - 1;
+
+    for (let j = i + 1; j < N; j++) {
+      if (tapedBase >= numberList[j]) {
+        i += 1;
+        isCovered = true;
+      } else {
+        continue;
+      }
+    }
+  }
+
+  return answer;
+}
+
+const response = main(N, L, numbers);
 console.log(response);

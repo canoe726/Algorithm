01c79387cea142a9cbfa95011d95fccc688aab4f
canoe726 <canoe918@gmail.com>
2024-05-21 00:28:45 +0900

    feat: zigzag conversion

---
+++ leetcode/top-interview-150/array_string/zigzag_conversion_2.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  if (numRows <= 1) {
+    return s;
+  }
+  let zigzag = new Array(numRows).fill('');
+  let zIndex = 0;
+  let plus = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    zigzag[zIndex] += s[i];
+    zIndex += plus;
+
+    if (zIndex < 0) {
+      plus = 1;
+      zIndex = 1;
+    }
+    if (zIndex >= numRows) {
+      plus = -1;
+      zIndex = numRows - 2;
+    }
+  }
+
+  return zigzag.reduce((acc, cur) => acc + cur, '');
+};
+
+console.log(convert('PAYPALISHIRING', 4));

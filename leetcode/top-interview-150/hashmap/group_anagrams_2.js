d2076118ccb991fa6aee239256e9bcc6bb308a33
canoe726 <canoe918@gmail.com>
2024-06-13 23:56:49 +0900

    feat: group anagram 2

--- leetcode/top-interview-150/hashmap/group_anagrams_2.js
+++ leetcode/top-interview-150/hashmap/group_anagrams_2.js
@@ -1,40 +1,42 @@
+function getUnique(str) {
+  let alpha = Array(26).fill(0);
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    alpha[str.charCodeAt(i) - 'a'.charCodeAt(0)] += 1;
+  }
+
+  for (let i = 0; i < alpha.length; i++) {
+    if (alpha[i] > 0) {
+      result += `${String.fromCharCode(i + 'a'.charCodeAt(0))}${alpha[i]}`;
+    }
+  }
+
+  return result;
+}
+
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  const sortedStrs = strs
-    .map((str, index) => {
-      return [str.split('').sort().join(''), index];
-    })
-    .sort();
+  const result = [];
+  const word = new Map();
 
-  const answer = [];
+  for (let i = 0; i < strs.length; i++) {
+    const unique = getUnique(strs[i]);
 
-  let i;
-  let temp = [];
-  for (i = 0; i < sortedStrs.length - 1; i++) {
-    if (sortedStrs[i][0] !== sortedStrs[i + 1][0]) {
-      temp.push(strs[sortedStrs[i][1]]);
-      answer.push(temp);
-      temp = [];
-    } else {
-      temp.push(strs[sortedStrs[i][1]]);
+    if (!word.has(unique)) {
+      word.set(unique, []);
     }
+    word.get(unique).push(strs[i]);
   }
 
-  if (i - 1 >= 0) {
-    if (sortedStrs[i - 1][0] !== sortedStrs[i][0]) {
-      answer.push([strs[sortedStrs[i][1]]]);
-    } else {
-      temp.push(strs[sortedStrs[i][1]]);
-      answer.push(temp);
-    }
-  } else {
-    answer.push(strs);
+  for (let [_, value] of word) {
+    result.push(value);
   }
 
-  return answer;
+  return result;
 };
 
-console.log(groupAnagrams([' ', 'a', '', 'a', 'a']));
+console.log(groupAnagrams(['d', 'az', 'b', 'd', 'za', 'a']));
